b831148f229935c1092fd7d2003d73db5884526f
ismaahmadmad <ismaa7990@gmail.com>
2024-08-18 15:05:45 +0500

    import_export_data

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 // app.js
 const express = require('express');
+const app = express();
+const PORT = 4000;
 const sequelize = require('./db');
 const excelRoutes = require('./routes/fileRouter');
 
-const app = express();
-const PORT = 4000;
+app.use(express.json());
+app.use('/', excelRoutes);
 
 sequelize.sync().then(() => {
   console.log('Database & tables created!');
 });
-
-
-app.use(express.json());
-app.use('/', excelRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
